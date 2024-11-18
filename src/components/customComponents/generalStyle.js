48cfac97e38e5cd005f7759aae06cce3bff8102d
NithisSelvam-ZS <nithis.selvam@zucisystems.com>
2024-11-18 11:17:06 +0530

    Implemented BreadCrumbs

--- src/components/customComponents/generalStyle.js
+++ src/components/customComponents/generalStyle.js
@@ -1,7 +1,48 @@
 const generalStyle = {
+  container: {
+    display: "flex",
+    width: "100%",
+    height: "100vh",
+    flexWrap: "nowrap",
+  },
   sidebar: {
     width: "260px",
     flexShrink: 0,
+    "@media (max-width: 650px)": {
+      display: "none",
+    },
+    "@media (max-width: 899px)": {
+      width: "200px", // For small screens
+    },
+    "@media (min-width: 900px) and (max-width: 1079px)": {
+      width: "200px", // For medium screens
+    },
+    "@media (min-width: 1080px) and (max-width: 1439px)": {
+      width: "260px", // For large screens
+    },
+    "@media (min-width: 1440px)": {
+      width: "260px", // For extra-large screens
+    },
+  },
+  children: {
+    flexGrow: 1,
+    flexShrink: 0,
+    width: "calc(100% - 260px)",
+    "@media (max-width: 650px)": {
+      width: "200px", // Adjust for small screens
+    },
+    "@media (max-width: 899px)": {
+      width: "400px", // Adjust for small screens
+    },
+    "@media (min-width: 900px) and (max-width: 1079px)": {
+      width: "600px", // Adjust for medium screens
+    },
+    "@media (min-width: 1080px) and (max-width: 1439px)": {
+      width: "800px", // Adjust for large screens
+    },
+    "@media (min-width: 1440px)": {
+      width: "1000px", // For extra-large screens
+    },
   },
 };
 export default generalStyle;
--- src/components/pages/accountManagement/accountManagement.js
+++ src/components/pages/accountManagement/accountManagement.js
@@ -1,6 +1,46 @@
 import React from "react";
+import Grid from "@mui/material/Grid2";
+import Breadcrumb from "../../customComponents/breadcrumb/Breadcrumb";
+import { globalMessages } from "../../../util/constant/StringConstants";
+import { routingUrl } from "../../../util/constant/UrlConstants";
+import { Card, CardContent, Typography } from "@mui/material";
+import accountManagementStyles from "./accountManagementStyle";
 
 const AccountManagement = () => {
-  return <span>Welcome to Account Management</span>;
+  const breadcrumbsData = [
+    {
+      label: globalMessages.home,
+      href: routingUrl.dashboard,
+    },
+    {
+      href: "",
+      label: globalMessages.newAccountManagement,
+    },
+  ];
+
+  return (
+    <Grid container mr={1.5}>
+      <Grid size={12} mt={2}>
+        <Breadcrumb items={breadcrumbsData} />
+      </Grid>
+      <Grid size={12} mt={1}>
+        <Typography sx={accountManagementStyles.heading}>{globalMessages.accountManagement}</Typography>
+      </Grid>
+      <Grid size={12} mt={2}>
+        {/* Need to add Table content */}
+        <Card>
+          <CardContent>
+            <Typography gutterBottom variant="h5" component="div">
+              Lizard
+            </Typography>
+            <Typography variant="body2" sx={{ color: "text.secondary" }}>
+              Lizards are a widespread group of squamate reptiles, with over 6,000 species, ranging across all
+              continents except Antarctica
+            </Typography>
+          </CardContent>
+        </Card>
+      </Grid>
+    </Grid>
+  );
 };
 export default AccountManagement;
