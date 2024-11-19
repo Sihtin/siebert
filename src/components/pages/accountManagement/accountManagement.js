482e7d9648650e1ed68d3f05dc83cea14515bea5
NithisSelvam-ZS <nithisselvam01@gmail.com>
2024-11-19 23:23:39 +0530

    Implemented Reducer for AccountManagement

--- src/components/pages/accountManagement/accountManagement.js
+++ src/components/pages/accountManagement/accountManagement.js
@@ -1,4 +1,4 @@
-import React from "react";
+import React, { useEffect } from "react";
 import Grid from "@mui/material/Grid2";
 import Breadcrumb from "../../customComponents/breadcrumb/Breadcrumb";
 import { globalMessages } from "../../../util/constant/StringConstants";
@@ -10,9 +10,18 @@ import NotificationsNoneOutlinedIcon from "@mui/icons-material/NotificationsNone
 import { assetKeys } from "../../../util/constant/AssetsConstants";
 import TableUI from "../../customComponents/table/TableUI";
 import { accountManagementConfig } from "./accountManagementConfig";
-import { accountData } from "./emptyAccountData";
+import { useSelector, useDispatch } from "react-redux";
+import { getAccountList } from "../../../redux/reducer/AccountListReducer";
+import CenterCircularBar from "../../customComponents/centercircularbar/CenterCircularBar";
 
 const AccountManagement = () => {
+  const accountListData = useSelector((state) => state.accountList);
+  const dispatch = useDispatch();
+
+  useEffect(() => {
+    dispatch(getAccountList());
+  }, []);
+
   const breadcrumbsData = [
     {
       label: globalMessages.home,
@@ -31,48 +40,56 @@ const AccountManagement = () => {
   const accountTableData = {
     tableHeaders: accountManagementConfig.tableHeaders,
     tableFields: accountManagementConfig.tableFields,
-    tableContents: accountData,
+    tableContents: accountListData ? accountListData?.data : [],
   };
 
   return (
-    <Grid container mr={1.5}>
-      <Grid size={12} mt={2}>
-        <Breadcrumb items={breadcrumbsData} />
-      </Grid>
-      <Grid size={12} mt={1}>
-        <Typography sx={accountManagementStyles.heading}>{globalMessages.accountManagement}</Typography>
-      </Grid>
-      <Grid size={12} mt={2}>
-        {/* Need to add Table content */}
-        <Card sx={{ height: "80vh" }}>
-          <CardContent>
-            <Grid container gap={2}>
-              <Grid>
-                <ButtonWithIcon
-                  onClick={handleSendEmail}
-                  sx={accountManagementStyles.sendEmailButtonStyle}
-                  startIcon={<NotificationsNoneOutlinedIcon sx={{ mr: -0.8, mt: -0.3 }} />}
-                >
-                  {globalMessages.sendEmail}
-                </ButtonWithIcon>
-              </Grid>
-              <Grid>
-                <ButtonWithIcon
-                  onClick={handleSendEmail}
-                  sx={accountManagementStyles.sendEmailButtonStyle}
-                  startIcon={<img src={assetKeys.sendMailIcon} alt="Send Mail Icon" width={"16px"} height={"16px"} />}
-                >
-                  {globalMessages.printMail}
-                </ButtonWithIcon>
-              </Grid>
-            </Grid>
-            <Grid mt={3}>
-              <TableUI tableData={accountTableData} tableHeight={"55vh"} />
-            </Grid>
-          </CardContent>
-        </Card>
-      </Grid>
-    </Grid>
+    <>
+      {accountListData?.isLoading && !accountListData?.isFetched ? (
+        <CenterCircularBar />
+      ) : (
+        <Grid container mr={1.5}>
+          <Grid size={12} mt={2}>
+            <Breadcrumb items={breadcrumbsData} />
+          </Grid>
+          <Grid size={12} mt={1}>
+            <Typography sx={accountManagementStyles.heading}>{globalMessages.accountManagement}</Typography>
+          </Grid>
+          <Grid size={12} mt={2}>
+            {/* Need to add Table content */}
+            <Card sx={{ height: "80vh" }}>
+              <CardContent>
+                <Grid container gap={2}>
+                  <Grid>
+                    <ButtonWithIcon
+                      onClick={handleSendEmail}
+                      sx={accountManagementStyles.sendEmailButtonStyle}
+                      startIcon={<NotificationsNoneOutlinedIcon sx={{ mr: -0.8, mt: -0.3 }} />}
+                    >
+                      {globalMessages.sendEmail}
+                    </ButtonWithIcon>
+                  </Grid>
+                  <Grid>
+                    <ButtonWithIcon
+                      onClick={handleSendEmail}
+                      sx={accountManagementStyles.sendEmailButtonStyle}
+                      startIcon={
+                        <img src={assetKeys.sendMailIcon} alt="Send Mail Icon" width={"16px"} height={"16px"} />
+                      }
+                    >
+                      {globalMessages.printMail}
+                    </ButtonWithIcon>
+                  </Grid>
+                </Grid>
+                <Grid mt={3}>
+                  <TableUI tableData={accountTableData} tableHeight={"55vh"} />
+                </Grid>
+              </CardContent>
+            </Card>
+          </Grid>
+        </Grid>
+      )}
+    </>
   );
 };
 export default AccountManagement;
--- src/redux/store/Store.js
+++ src/redux/store/Store.js
@@ -1,8 +1,10 @@
 import { configureStore } from "@reduxjs/toolkit";
 import { selectedPathReducer } from "../reducer/SidebarReducer";
+import { accountListReducer } from "../reducer/AccountListReducer";
 
 export const store = configureStore({
   reducer: {
     selectedPath: selectedPathReducer,
+    accountList: accountListReducer,
   },
 });
